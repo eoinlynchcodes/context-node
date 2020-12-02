608ed588ef89979e85fa08ffd43386f3c00df226
eoinlynchcodes <eoin95lynch@gmail.com>
2020-12-02 21:13:57 +0000

    Go

--- api/server.js
+++ api/server.js
@@ -2,16 +2,38 @@ const express = require("express");
 const cors = require('cors');
 const helmet = require('helmet');
 
-const server = express();
+const prospectControllers = require('../prospects/prospectControllers.js');
+const router = require('express').Router();
 
-const prospectRoutes = require('../prospects/prospectControllers.js');
+const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-server.use('/api/prospects', prospectRoutes);
+server.get('/', (req, res) => {
+    prospectControllers.findAll()
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.status(400).json(error);
+    });
+})
 
+server.post('/', (req, res) => {
+    let { name, email } = req.body;
+    prospectControllers.add({
+        name,
+        email
+    })
+    .then(newUser => {
+        res.status(201).json(newUser);
+    })
+    .catch(error => {
+        res.status(500).json('There is an error' + error);
+    });
+});
 server.get("/", (req, res) => {
     res.send("<h1>Hello</h1>")
 });
--- prospects/prospectControllers.js
+++ prospects/prospectControllers.js
@@ -14,3 +14,4 @@ async function add(user){
 function getAll(){
     return db('users');
 }
+
