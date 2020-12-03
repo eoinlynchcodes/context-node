cbaba8e4c425d7a27c0ecaa77eaae69be0956b0c
eoinlynchcodes <eoin95lynch@gmail.com>
2020-12-03 16:00:16 +0000

    Go

--- database/migrations/roundone.js
+++ database/migrations/roundone.js
@@ -1,13 +1,14 @@
-exports.up = function (knex) {
+
+export function up(knex) {
   return knex.schema
   .createTable('users', table => {
       table.increments();
       table.text('name');
       table.text('email');
   });
-};
+}
 
-exports.down = function (knex) {
+export function down(knex) {
     return knex.schema
     .dropTableIfExists('users');
-};
+}
