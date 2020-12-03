11521287cad629367fc23bf7426df0dedd2df7d2
eoinlynchcodes <eoin95lynch@gmail.com>
2020-12-03 16:40:49 +0000

    Deleted development from knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/test.db3'
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations',
-    },
-    seeds: {
-      directory: './database/seeds'
-    },
-  },
-
   production: {
     client: 'pg',
     connection: process.env.REACT_APP_DATABASE_URL,
