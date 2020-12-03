948b28676ace7c32278b03849795bf85c15a1eec
eoinlynchcodes <eoin95lynch@gmail.com>
2020-12-03 16:57:02 +0000

    dbConfig updated

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const config = require('./knexfile.js');
 
 // we must select the development object from our knexfile
-const db = knex(config.development);
+const dbEnv = process.env.dbEnv || 'development';
 
 // export for use in codebase
-module.exports = db;
+module.exports = knex(knexConfig[dbEnv])
